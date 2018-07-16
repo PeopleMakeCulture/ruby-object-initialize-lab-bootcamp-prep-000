e45659b02510e0023d783573e7d3dc57cd675af4
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 22:09:46 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,12 +12,12 @@ class Dog
     @name
   end
 
-  def breed=(dogs_breed)
-    @this_dogs_breed = dogs_breed
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
-    @this_dogs_breed
+    @breed
   end
 
 end
