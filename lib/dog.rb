6e49ef9c408b26f71eeb623514f25313d20c2dc4
Jing Cao <jingcao.yale@gmail.com>
2018-07-16 22:12:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 class Dog
   def initialize (name)#,breed)
-    @name = name
+    @name= name
     #@breed = breed
   end
 
   def name=(name)
-    @name = name
+    @name= name
   end
 
   def name
